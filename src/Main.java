7dd4ea76316ccaec9cdc8329bcb1e50b07a074f5
qwsq4 <qwsq1437@gmail.com>
2022-11-13 17:56:16 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+
+    }
+}
