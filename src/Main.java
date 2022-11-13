fd78c1fd9087dd8f38c514f91e8d89cb15ae299d
qwsq4 <qwsq1437@gmail.com>
2022-11-13 17:57:24 +0800

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,100 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
+    private static void inputTask(Scanner scanner) throws ParseException {
+        System.out.print("Введите название задачи: ");
+        String taskName = scanner.next();
+        System.out.print("Введите описание задачу: ");
+        String taskDesc = scanner.next();
+        System.out.print("Укажите дату выполнения(в формате день.месяц.год): ");
+        String stringDate = scanner.next();
+        Date date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat();
+        format.applyPattern("dd.MM.yyyy");
+        date = format.parse(stringDate);
+        System.out.print("Укажите время выполнения(час в 24-часовом формате): ");
+        date.setHours(scanner.nextInt());
+        System.out.print("Укажите тип задачи(1 - если личная, 2 - если рабочая, по умолчанию выставляется личная): ");
+        int taskType = scanner.nextInt();
+        System.out.print("Укажите повторяемость задачи(0 - неповторяемая, 1 - ежедневная, 2 - еженедельная, " +
+                "3 - ежемесячная, 4 - ежегодная): ");
+        int taskRepeatability = scanner.nextInt();
+        switch (taskRepeatability) {
+            case 0:
+                Task.addToList(new Task<>(taskName, taskDesc, taskType, date));
+                break;
+            case 1:
+                Task.addToList(new Task<Task.DailyRepeat>(taskName, taskDesc, taskType, date));
+                break;
+            case 2:
+                Task.addToList(new Task<Task.WeeklyRepeat>(taskName, taskDesc, taskType, date));
+                break;
+            case 3:
+                Task.addToList(new Task<Task.MonthlyRepeat>(taskName, taskDesc, taskType, date));
+                break;
+            case 4:
+                Task.addToList(new Task<Task.YearlyRepeat>(taskName, taskDesc, taskType, date));
+                break;
+        }
+        System.out.print("Задача " + taskName + " добавлена!");
+    }
+
+    public static void deleteTask(Scanner scanner) {
+        System.out.print("Введите ID задачи: ");
+        Task.deleteFromList(scanner.nextInt());
+    }
+
+    public static void checkTasks(Scanner scanner) throws ParseException{
+        System.out.print("Введите дату: ");
+        String stringDate = scanner.next();
+        Date date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat();
+        format.applyPattern("dd.MM.yyyy");
+        date = format.parse(stringDate);
+        Task.checkTasks(date);
+    }
+
+    private static void printMenu() {
+        System.out.println("1. Добавить задачу");
+        System.out.println("2. Удалить задачу");
+        System.out.println("3. Получить задачу на указанный день");
+        System.out.println("0. Выход");
+    }
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            label:
+            while (true) {
+                printMenu();
+                System.out.print("Выберите пункт меню: ");
+                if (scanner.hasNextInt()) {
+                    int menu = scanner.nextInt();
+                    switch (menu) {
+                        case 1:
+                            try {
+                                inputTask(scanner);
+                                break;
+                            } catch (ParseException e) {
+                                System.out.println("Не удалось добавить задачу");
+                            }
+                        case 2:
+                            deleteTask(scanner);
+                            break;
+                        case 3:
+                            try {
+                                checkTasks(scanner);
+                            } catch (ParseException e) {
+                                System.out.println("Не удалось получить задачи на указанный день");
+                            }
+                            break;
+                        case 0:
+                            break label;
+                    }
+                } else {
+                    scanner.next();
+                    System.out.println("Выберите пункт меню из списка!");
+                }
+            }
+        }
     }
 }
---
+++ src/Task.java
@@ -0,0 +1,143 @@
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task <T extends Task & Repeatability> {
+
+    public class DailyRepeat extends Task implements Repeatability{
+        public DailyRepeat(String name, String desc, int type, Date date) {
+            super(name, desc, type, date);
+        }
+        private boolean needDoToday(Date checkDay) {
+            return date.getHours() <= checkDay.getHours();
+        }
+    }
+
+    public class WeeklyRepeat extends Task implements Repeatability {
+        public WeeklyRepeat(String name, String desc, int type, Date date) {
+            super(name, desc, type, date);
+        }
+        private boolean needDoToday(Date checkDay) {
+            return getDayNumber(date) == getDayNumber(checkDay) &&
+                    date.getHours() <= checkDay.getHours();
+        }
+    }
+
+    public class MonthlyRepeat extends Task implements Repeatability {
+        public MonthlyRepeat(String name, String desc, int type, Date date) {
+            super(name, desc, type, date);
+        }
+        private boolean needDoToday(Date checkDay) {
+            return date.getDay() == checkDay.getDay() &&
+                    date.getHours() <= checkDay.getHours();
+        }
+    }
+
+    public class YearlyRepeat extends Task implements Repeatability{
+        public YearlyRepeat(String name, String desc, int type, Date date) {
+            super(name, desc, type, date);
+        }
+        private boolean needDoToday(Date checkDay) {
+            return date.getMonth() == checkDay.getMonth() &&
+                    date.getDay() == checkDay.getDay() &&
+                    date.getHours() <= checkDay.getDay();
+        }
+    }
+
+    private String name;
+    private String desc;
+    private String type;
+    private Date date;
+    private static int count = 0;
+    private final int id;
+    private static Map<Integer, Task<?>> taskList = new HashMap<>();
+
+    public Task(String name, String desc, int type, Date date) {
+        this.name = name;
+        this.desc = desc;
+        if (type < 3) {
+            switch (type) {
+                case 1:
+                    this.type = "Личная";
+                    break;
+                case 2:
+                    this.type = "Рабочая";
+                    break;
+            }
+        } else this.type = "Не указано";
+        this.date = date;
+        this.id = count;
+        count++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static Map<Integer, Task<?>> getTaskList() {
+        return taskList;
+    }
+
+    private boolean needDoToday(Date checkDay) {
+        return date.getYear() == checkDay.getYear() &&
+                date.getMonth() == checkDay.getMonth() &&
+                date.getDay() == checkDay.getDay() &&
+                date.getHours() <= checkDay.getDay();
+    }
+
+    public static void checkTasks(Date date) {
+        System.out.println("Задачи на этот день");
+        for (Task task : taskList.values()) {
+            if (task.needDoToday(date));
+            System.out.println(task.getName() + ", " + task.getId());
+        }
+    }
+
+    public static void addToList(Task<?> task) {
+        taskList.put(task.getId(), task);
+    }
+
+    public static void deleteFromList(int id) {
+        if (taskList.containsKey(id)) {
+            taskList.remove(id);
+            System.out.println("Задача " + taskList.get(id).getName() + " удалена!");
+        } else System.out.println("Такой задачи нет");
+    }
+
+    public static int getDayNumber(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        return cal.get(Calendar.DAY_OF_WEEK);
+    }
+}
